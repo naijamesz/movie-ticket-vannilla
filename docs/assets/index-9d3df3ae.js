f82d0b53680828910c900d8f466ba522ad42d99f
naijamesz <kodlikejamesz@hotmail.com>
2023-09-05 13:24:33 +0700

    Complete build.

--- docs/assets/index-9d3df3ae.js
+++ docs/assets/index-9d3df3ae.js
@@ -1,4 +1,33 @@
-(function(){const d=document.createElement("link").relList;if(d&&d.supports&&d.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))e(i);new MutationObserver(i=>{for(const a of i)if(a.type==="childList")for(const t of a.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&e(t)}).observe(document,{childList:!0,subtree:!0});function v(i){const a={};return i.integrity&&(a.integrity=i.integrity),i.referrerPolicy&&(a.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?a.credentials="include":i.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function e(i){if(i.ep)return;i.ep=!0;const a=v(i);fetch(i.href,a)}})();document.querySelector("#app").innerHTML=`
+(function () {
+  const d = document.createElement('link').relList;
+  if (d && d.supports && d.supports('modulepreload')) return;
+  for (const i of document.querySelectorAll('link[rel="modulepreload"]')) e(i);
+  new MutationObserver(i => {
+    for (const a of i)
+      if (a.type === 'childList')
+        for (const t of a.addedNodes) t.tagName === 'LINK' && t.rel === 'modulepreload' && e(t);
+  }).observe(document, { childList: !0, subtree: !0 });
+  function v(i) {
+    const a = {};
+    return (
+      i.integrity && (a.integrity = i.integrity),
+      i.referrerPolicy && (a.referrerPolicy = i.referrerPolicy),
+      i.crossOrigin === 'use-credentials'
+        ? (a.credentials = 'include')
+        : i.crossOrigin === 'anonymous'
+        ? (a.credentials = 'omit')
+        : (a.credentials = 'same-origin'),
+      a
+    );
+  }
+  function e(i) {
+    if (i.ep) return;
+    i.ep = !0;
+    const a = v(i);
+    fetch(i.href, a);
+  }
+})();
+document.querySelector('#app').innerHTML = `
 <div class="movie-container">
 <label>Pick a movie:</label>
 <select id="movie">
@@ -307,4 +336,40 @@
 <div class="label-screen">
     <p class="text">You have selected <span id="count">0</span> seats for a price of $<span id="total">0</span></p>
 </div>
-`;const r=document.querySelector(".container"),o=document.querySelectorAll(".row .seat:not(.occupied)"),u=document.getElementById("count"),p=document.getElementById("total"),l=document.getElementById("movie");f();let n=+l.value;function m(s,d){localStorage.setItem("selectedMovieIndex",s),localStorage.setItem("selectedMoviePrice",d)}function c(){const s=document.querySelectorAll(".row .seat.selected"),d=[...s].map(e=>[...o].indexOf(e));localStorage.setItem("selectedSeats",JSON.stringify(d));const v=s.length;u.innerText=v,p.innerText=v*n}function f(){const s=JSON.parse(localStorage.getItem("selectedSeats"));s!==null&&s.length>0&&o.forEach((v,e)=>{s.indexOf(e)>-1&&v.classList.add("selected")});const d=localStorage.getItem("selectedMovieIndex");d!==null&&(l.selectedIndex=d)}l.addEventListener("change",s=>{n=+s.target.value,m(s.target.selectedIndex,s.target.value),c()});r.addEventListener("click",s=>{s.target.classList.contains("seat")&&!s.target.classList.contains("occupied")&&(s.target.classList.toggle("selected"),c())});c();
+`;
+const r = document.querySelector('.container'),
+  o = document.querySelectorAll('.row .seat:not(.occupied)'),
+  u = document.getElementById('count'),
+  p = document.getElementById('total'),
+  l = document.getElementById('movie');
+f();
+let n = +l.value;
+function m(s, d) {
+  localStorage.setItem('selectedMovieIndex', s), localStorage.setItem('selectedMoviePrice', d);
+}
+function c() {
+  const s = document.querySelectorAll('.row .seat.selected'),
+    d = [...s].map(e => [...o].indexOf(e));
+  localStorage.setItem('selectedSeats', JSON.stringify(d));
+  const v = s.length;
+  (u.innerText = v), (p.innerText = v * n);
+}
+function f() {
+  const s = JSON.parse(localStorage.getItem('selectedSeats'));
+  s !== null &&
+    s.length > 0 &&
+    o.forEach((v, e) => {
+      s.indexOf(e) > -1 && v.classList.add('selected');
+    });
+  const d = localStorage.getItem('selectedMovieIndex');
+  d !== null && (l.selectedIndex = d);
+}
+l.addEventListener('change', s => {
+  (n = +s.target.value), m(s.target.selectedIndex, s.target.value), c();
+});
+r.addEventListener('click', s => {
+  s.target.classList.contains('seat') &&
+    !s.target.classList.contains('occupied') &&
+    (s.target.classList.toggle('selected'), c());
+});
+c();
